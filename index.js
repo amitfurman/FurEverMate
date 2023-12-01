90759c35d867d1c1f8d1f8fdac7eaf8a517ba94c
amitfu <amitff103@gmail.com>
2023-12-01 16:17:16 +0200

    favorites

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs/promises');
 
+const favoriteCats = [];
 async function main() {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
@@ -26,12 +27,48 @@ async function main() {
     //     return imgs.map(img => img.src)        
     // })
 
-    // for (const img of catsImages) {
-    //     const imagePage = await page.goto(img)
-    //     await fs.writeFile(img.split("/").pop(), await imagePage.buffer())
+
+    // for (const cat of catsImages) {
+    //     const imagePage = await page.goto(cat)
+    //     await fs.writeFile(cat.split("/").pop(), await imagePage.buffer())
     // }
 
+      // Wait for the element that contains the dynamically loaded images
+
+    const catsImages = await page.$$eval('img', (imgs) => {
+        return imgs
+          .filter((img) => img.src && img.src.toLowerCase().startsWith('http'))
+          .map((img) => img.src);
+      });
+      
+      
+      for (const catUrl of catsImages) {
+        try {
+          const imagePage = await page.goto(catUrl);
+          const fileName = catUrl.split("/").pop(); // Extract file name (optional)
+          await fs.writeFile(fileName, await imagePage.buffer());
+          console.log(`Image ${fileName} saved successfully.`);
+        } catch (error) {
+          console.error(`Error fetching or saving image from URL: ${catUrl}`);
+          console.error(error);
+        }
+      }
+      
+      
     await browser.close()
 }
+
+function toggleHeart(catId) {
+  var heartIcon = document.getElementById('heart-' + catId);
+  if (heartIcon.classList.contains('fas')) {
+    heartIcon.classList.replace('fas', 'far');
+    heartIcon.classList.remove('red-heart');
+    favoriteCats.push(catId);
+  } else {
+    heartIcon.classList.replace('far', 'fas');
+    heartIcon.classList.add('red-heart');
+  }
+}
+
 
 main().catch(error => console.error(error));
