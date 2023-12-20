18dd80216d1538fa6d2a8a1363a480a3f8d1ff91
amitfu <amitff103@gmail.com>
2023-12-20 20:24:43 +0200

    add filter by area

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
@@ -17,6 +18,7 @@ app.get('/', async (req, res) => {
         // Combine the data from both sources
         const combinedData = sospetsData.concat(letliveData);
 
+        fs.writeFileSync('combinedData.json', JSON.stringify(combinedData, null, 2), 'utf-8');
         // Render the index.ejs template with the combined data
         res.render('index', { cardDetails: combinedData });
     } catch (error) {
@@ -24,6 +26,18 @@ app.get('/', async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
+
+app.get('/combinedData', (req, res) => {
+    try {
+        const rawData = fs.readFileSync('combinedData.json', 'utf-8');
+        const data = JSON.parse(rawData);
+        res.json(data);
+    } catch (error) {
+        console.error('Error reading combinedData.json:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 
 async function fetchDataFromSospets() {
     const browser = await puppeteer.launch();
@@ -111,43 +125,6 @@ async function fetchDataFromLetLive() {
             return { name, description: textLines[1] || '', imgSrc, location, isMale, href };
           });
           
-
-        // return cards.map(card => {
-        //     const imgElement = card.querySelector('img');
-        //     const imgSrc = imgElement ? imgElement.src : '';
-
-        //     const textLines = card.innerText.split('\n\n').slice(0, 2) || '';
-
-        //     let name = '';
-        //     const h3Element = document.querySelector('h3 a');
-        //     if (h3Element) {
-        //         const href = h3Element.getAttribute('href');
-        //         name = decodeURIComponent(new URL(href).searchParams.get('pet'));
-        //     }
-
-        //     const description = textLines[1] || '';
-
-        //     const petDetailsGenderElements = document.querySelectorAll('.pet-details-gendar');
-        //     let isMale = false;
-        //     let location = '';
-
-        //     // Iterate through the elements and check their content
-        //     petDetailsGenderElements.forEach(element => {
-        //     const textContent = element.innerText;
-
-        //     // Check if the element contains the word "זכר"
-        //     if (textContent.includes('זכר')) {
-        //         isMale = true;
-        //     }
-
-        //     // Check if the element contains specific location text
-        //     if (!textContent.includes('זכר') && !textContent.includes('נקבה')) {
-        //         location = textContent;
-        //     }
-        //     });
-
-        //     return { name, description, imgSrc, location, isMale };
-        // });
     });
 
     await browser.close();
@@ -159,212 +136,3 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// // Import the Puppeteer library
-// const puppeteer = require('puppeteer');
-// const express = require('express');
-// const app = express();
-// const PORT = 3000;
-
-// app.set('view engine', 'ejs');
-// app.use(express.static('public'));
-
-// app.get('/', async (req, res) => {
-//     try {
-//       const browser = await puppeteer.launch();
-//       const page = await browser.newPage();
-//       await page.goto('https://www.sospets.co.il/cats-adoption', { timeout: 20000 });
-  
-//       const cardDetails = await page.evaluate(() => {
-//         const cards = Array.from(document.querySelectorAll('.Zc7IjY'));
-  
-//         return cards.map(card => {
-//           const imgElement = card.querySelector('img');
-//           const imgSrc = imgElement ? imgElement.src : '';
-  
-//           const textLines = card.innerText.split('\n\n').slice(0, 2);
-//           const name = textLines[0] || '';
-//           const description = textLines[1] || '';
-  
-//           const location = 'SOS הרצליה';
-//           const isMale = description.includes('בן');
-//           return { name, description, imgSrc, location, isMale };
-//         });
-//       });
-  
-//       await browser.close();
-  
-//       // Render the index.ejs template with the cardDetails
-//       res.render('index', { cardDetails });
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).send('Internal Server Error');
-//     }
-//   });
-
-//   app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-//   });
- 
-// // Define an asynchronous function to run our code
-// (async () => {
-//     // Launch a new browser instance using Puppeteer
-//     const browser = await puppeteer.launch();
- 
-//     // Open a new page in the browser
-//     const page = await browser.newPage();
- 
-//     // Navigate to the specified URL
-//     await page.goto('https://www.sospets.co.il/cats-adoption', { timeout: 20000 });
- 
-//     // Extract details from each card on the page
-//     const cardDetails = await page.evaluate(() => {
-//         // Step 1: Retrieve all card elements
-//         const cards = Array.from(document.querySelectorAll('.Zc7IjY'));
- 
-//          // Step 2: Dynamically create cards based on the data
-//         const catContainer = document.querySelector('.cats-container');
-//         const cardTemplate = document.getElementById('catCardTemplate'); 
-
-
-//         // Step 3: Process each card to extract details
-//         return cards.map(card => {
-//             // Find the image element in the card and get its source (src) attribute
-//             const imgElement = card.querySelector('img');
-//             const imgSrc = imgElement ? imgElement.src : '';
- 
-//             // Split the text inside the card into lines and take the first two lines
-//             const textLines = card.innerText.split('\n\n').slice(0, 2);
-//             const name = textLines[0] || '';
-//             const description = textLines[1] || '';
-
-//                     // Clone the template
-//             const cardClone = cardTemplate ? document.importNode(cardTemplate.content, true) : null;
-
-//             // Update the content of the cloned template with data
-//             cardClone.querySelector('img').src = card.imgSrc;
-//             cardClone.querySelector('h5').textContent = card.name;
-//             cardClone.querySelector('.description h3 p').textContent = card.description;
-
-//             // Append the cloned template to the cat container
-//             catContainer.appendChild(cardClone);
- 
-//             // Return an object containing the name, description, and image source for each card
-//             return { name, description, imgSrc };
-//         });
-//     });
- 
-//     // Close the browser after finishing
-//     await browser.close();
-// })();
-
-
-
-
-
-// const puppeteer = require('puppeteer');
-// const fs = require('fs/promises');
-
-// const favoriteCats = [];
-// async function main() {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     const url = 'https://www.sospets.co.il/cats-adoption';
-//     await page.goto(url, {'timeout': 300000});
-    
-//     const catsarray = await page.evaluate(() => {
-//         const cats = document.querySelectorAll('[id^="comp-kcbmlyum__"]');
-//         const catsArray = [];
-//         for (const cat of cats) {
-//           console.log("Cat Element:", cat);
-//           const catObj = {};
-//           catObj.id = cat.id;
-//           console.log(cat.getAttribute('title'));
-//           catObj.name = cat.getAttribute('title');
-//           catObj.image = cat.querySelector('img').src;
-//           catsArray.push(catObj);
-//         }
-//         return catsArray;
-//     })
-
-//      console.log(catsarray);
-
-//     // //extract cats names
-//     // const catsNames = await page.evaluate(() => {
-//     //     const elements = document.querySelectorAll('[id^="comp-kcbmlyum__"]');
-//     //     const titles = [];
-//     //     elements.forEach((element) => {
-//     //         const title = element.getAttribute('title');
-//     //         if (title) {
-//     //             titles.push(title);
-//     //          }
-//     //     });
-//     //     return titles;
-//     // })
-//     // await fs.writeFile('catsNames.txt', catsNames.join())
-
-//     //   // Extract image URLs
-//     // const imageUrls = await page.evaluate(() => {
-//     //   const images = document.querySelectorAll("[id^='comp-kcbmlyum__'] img");
-//     //   const urls = Array.from(images).map(img => img.src);
-//     //   return urls;
-//     // });
-
-//     await browser.close()
-// }
-
-// main().catch(error => console.error(error));
-
-
-
-    
-//     // const catsImages = await page.$$eval("img" , (imgs)=> {
-//     //     return imgs.map(img => img.src)        
-//     // })
-
-
-//     // for (const cat of catsImages) {
-//     //     const imagePage = await page.goto(cat)
-//     //     await fs.writeFile(cat.split("/").pop(), await imagePage.buffer())
-//     // }
-
-//       // Wait for the element that contains the dynamically loaded images
-
-//     // const catsImages = await page.$$eval('img', (imgs) => {
-//     //     return imgs
-//     //       .filter((img) => img.src && img.src.toLowerCase().startsWith('http'))
-//     //       .map((img) => img.src);
-//     //   });
-      
-      
-//     //   for (const catUrl of catsImages) {
-//     //     try {
-//     //       const imagePage = await page.goto(catUrl);
-//     //       const fileName = catUrl.split("/").pop(); // Extract file name (optional)
-//     //       await fs.writeFile(fileName, await imagePage.buffer());
-//     //       console.log(`Image ${fileName} saved successfully.`);
-//     //     } catch (error) {
-//     //       console.error(`Error fetching or saving image from URL: ${catUrl}`);
-//     //       console.error(error);
-//     //     }
-//     //   }
-      
-
-//     // const imageUrls = await page.evaluate(() => {
-//     //   const images = document.querySelectorAll('.HlRz5e.BI8PVQ[data-image-info][data-src^="https://static.wixstatic.com/"]:not([data-image-info*="284x397"])');
-
-
-//     //   const urls = [];
-//     //   images.forEach(function (element) {
-//     //       var nestedImgElement = element.querySelector('img');
-//     //       if (nestedImgElement) {
-//     //           var imageUrl = nestedImgElement.getAttribute('src');
-//     //           if (imageUrl)
-//     //             urls.push(imageUrl);
-//     //       } else {
-//     //           console.log('Nested img element not found inside an element with class HlRz5e');
-//     //       }
-//     //   });
-//     //   return urls;
-//     // });
-
-    // Import the Puppeteer library
--- public/utils.js
+++ public/utils.js
@@ -1,5 +1,3 @@
-console.log('Utils.js loaded!');
-
 
 function toggleHeart(catId) {
     console.log('toggleHeart called with catId:', catId);
@@ -13,3 +11,136 @@ function toggleHeart(catId) {
       heartIcon.classList.add('red-heart');
     }
   }
+
+  async function fetchAndFilterCards() {
+    try {
+        const response = await fetch('/combinedData'); // Now fetching from the new endpoint
+        const data = await response.json();
+
+        // Call sortAndFilterCards with the retrieved data
+        sortAndFilterCards(data);
+    } catch (error) {
+        console.error('Error fetching data from server:', error);
+    }
+}
+
+  
+  const locationMapping = {
+    'north': 'תנו לחיות לחיות צפון',
+    'center': ['SOS הרצליה', 'תנו לחיות לחיות מרכז'],
+    'south': 'תנו לחיות לחיות דרום',
+    // Add more locations if needed
+};
+
+function sortAndFilterCards(cardDetails) {
+    // Get the selected options from the checkboxes
+    const selectedOptions = getSelectedOptions();
+    console.log('Selected options:', selectedOptions);
+
+    // Map user-friendly names to actual locations
+   // const mappedOptions = selectedOptions.map(option => locationMapping[option]);
+   const mappedOptions = selectedOptions.flatMap(option => {
+    const mappedValue = locationMapping[option];
+  
+    if (Array.isArray(mappedValue)) {
+      return mappedValue;
+    } else if (mappedValue) {
+      return [mappedValue];
+    } else {
+      return [];
+    }
+  });
+    console.log('Mapped options:', mappedOptions);
+
+    // Filter the cards based on the selected options
+    const filteredCards = cardDetails.filter(card => {
+        if (mappedOptions.length === 0) {
+            return true; // Show all cards if no checkbox is selected
+        } else {
+            // Modify the condition based on your filtering logic
+            // Check if the card's location is included in the selected options
+            return mappedOptions.includes(card.location);
+        }
+    });
+
+    console.log('Filtered cards:', filteredCards);
+
+    // Clear existing cards
+    const cardsContainer = document.querySelector('.cats-container');
+    cardsContainer.innerHTML = '';
+
+    // Render the filtered cards
+    filteredCards.forEach((card, index) => {
+        const cardElement = createCardElement(card, index);
+        cardsContainer.appendChild(cardElement);
+    });
+
+}
+
+function getSelectedOptions() {
+    const checkboxes = Array.from(document.querySelectorAll('.filter-checkbox'));
+    const selectedOptions = checkboxes.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+
+    return selectedOptions;
+}
+
+// Helper function to create HTML elements for the cards
+function createCardElement(card, index) {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('row');
+
+    const cardLink = document.createElement('a');
+    cardLink.href = card.href;
+    cardLink.target = '_blank';
+    cardLink.classList.add('card-link');
+
+    const imgElement = document.createElement('img');
+    imgElement.src = card.imgSrc;
+    imgElement.alt = '';
+
+    cardLink.appendChild(imgElement);
+
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add('info');
+
+    const locationHeading = document.createElement('h5');
+    locationHeading.textContent = card.location;
+
+    const heartDiv = document.createElement('div');
+    const heartIcon = document.createElement('i');
+    heartIcon.id = `heart-cat${index + 1}`;
+    heartIcon.classList.add('far', 'fa-heart');
+    heartIcon.onclick = () => toggleHeart(`cat${index + 1}`);
+    heartDiv.appendChild(heartIcon);
+
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.classList.add('description');
+
+    const nameHeading = document.createElement('h3');
+    nameHeading.textContent = card.name;
+
+    const genderIcon = document.createElement('span');
+    genderIcon.classList.add(card.isMale ? 'male-sign' : 'female-sign');
+    const genderIconClass = card.isMale ? 'fas fa-mars' : 'fas fa-venus';
+    genderIcon.innerHTML = `<i class="${genderIconClass}"></i>`;
+
+    nameHeading.appendChild(genderIcon);
+
+    const descriptionParagraph = document.createElement('p');
+    descriptionParagraph.textContent = card.description;
+
+    descriptionDiv.appendChild(nameHeading);
+    descriptionDiv.appendChild(descriptionParagraph);
+
+    infoDiv.appendChild(locationHeading);
+    infoDiv.appendChild(heartDiv);
+    infoDiv.appendChild(descriptionDiv);
+
+    cardDiv.appendChild(cardLink);
+    cardDiv.appendChild(infoDiv);
+
+    return cardDiv;
+}
+
+
+
